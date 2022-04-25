057cfdf91277cf78d50d1c32a985c48e68194669
yousefdheede <58173109+yousefdheede@users.noreply.github.com>
2022-04-26 02:30:16 +0300

    Add files via upload

--- oreder_server/routes.py
+++ oreder_server/routes.py
@@ -1,39 +1,39 @@
-from flask_app import app, catalog_ip
+from flask_app import app, CATALOG_ADDRESS
 import requests
 
 
-########### BUY ###########
-
+# Buy endpoint
 @app.route('/buy/<book_id>', methods=['PUT'])
 def buy(book_id):
     # If the ID is not a number, reject the purchase
     if not book_id.isnumeric():
-        return {' ID must be a number'}, 422
+        return {'message': 'Book ID must be a number'}, 422
 
-    ############ search in catalog for book id ###########
-    book_response = requests.get(f'{catalog_ip}/query/item/{book_id}')
+    # Query the book from the catalog server
+    book_response = requests.get(f'{CATALOG_ADDRESS}/query/item/{book_id}')
 
-    ############ if book not found --> error ! ###########
+    # If the response status is 404 not found, override the error message
     if book_response.status_code == 404:
-        return {' ID does not fouund'}, 404
+        return {'message': 'Book with the specified ID does not exist'}, 404
 
-    ############ else , if not ok return another error ###########
+    # If any other non-OK response is received, return it as-is
     elif book_response.status_code != 200:
         return book_response.content, book_response.status_code, book_response.headers.items()
 
-    ############ get book info from response ###########
+    # Extract the book information from the response
     book = book_response.json()
 
-    ############ if theres no books in stock ###########
+    # If the quantity is 0, return that the book is out of stock
     if book['quantity'] <= 0:
-        return {'Book out of stock'}
+        return {'success': False, 'message': 'Book with the specified ID is out of stock'}
 
-    buy_response = requests.put(f'{catalog_ip}/update/{book_id}', json={'quantity': book['quantity']-1})
+    # Otherwise, update the book quantity on the catalog server using the update message
+    buy_response = requests.put(f'{CATALOG_ADDRESS}/update/{book_id}', json={'quantity': book['quantity']-1})
 
-    ############  errors in updating ###########
+    # If any error occurs while updating, return the error as-is
     if buy_response.status_code != 200:
         return buy_response.text, buy_response.status_code, buy_response.headers.items()
 
-    ############ else success ###########
-    return {'Book with ID purchased'}
+    # Otherwise, return a successful purchase message
+    return {'success': True, 'message': 'Book with the specified ID purchased'}
 
